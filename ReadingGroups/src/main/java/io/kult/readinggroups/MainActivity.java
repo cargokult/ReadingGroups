173ab438bc827f96bd9d176661b4574daad6c675
Cargo Kult <cargokultist@gmail.com>
2013-11-19 06:59:56 +0200

    renamed Books to MainActivity

--- ReadingGroups/src/main/java/io/kult/readinggroups/MainActivity.java
+++ ReadingGroups/src/main/java/io/kult/readinggroups/MainActivity.java
@@ -22,7 +22,7 @@ import com.firebase.client.ValueEventListener;
 
 import java.util.Locale;
 
-public class Books extends ActionBarActivity implements ActionBar.TabListener {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -42,7 +42,7 @@ public class Books extends ActionBarActivity implements ActionBar.TabListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_books);
+        setContentView(R.layout.activity_main);
 
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
@@ -101,7 +101,7 @@ public class Books extends ActionBarActivity implements ActionBar.TabListener {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.books, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -195,7 +195,7 @@ public class Books extends ActionBarActivity implements ActionBar.TabListener {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_books, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             //textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
